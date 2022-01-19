03826262ec446d5aae568dcdf6ffc99efcaa7c58
Ivon <ivonne@jjf.com>
2022-01-18 18:41:28 -0600

    add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
+
 import Home from './pages/Home';
 import Login from './pages/Login';
 import NoMatch from './pages/NoMatch';
@@ -26,17 +32,17 @@ function App() {
       <Router>
         <div className="flex-column justify-flex-start min-100-vh">
           <Header />
-			   <div className="container">
-			  <Switch>
-				<Route exact path="/" component={Home} />
-				<Route exact path="/login" component={Login} />
-				<Route exact path="/signup" component={Signup} />
-				<Route exact path="/profile" component={Profile} />
-				<Route exact path="/thought" component={SingleThought} />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/profile/:username?" component={Profile} />
+              <Route exact path="/thought/:id" component={SingleThought} />
 
-				<Route component={NoMatch} />
-			  </Switch>
-			</div>
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
           <Footer />
         </div>
       </Router>
